b12116ec6776e2fc49a2cbaf97fbf9323279b163
Pablo Ocampo <38480731+pablocampogo@users.noreply.github.com>
2023-04-05 15:59:46 -0400

    feat: add optional ssh support (#8)

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
 	"syscall"
@@ -12,6 +14,7 @@ import (
 	"github.com/pokt-foundation/transaction-http-db/router"
 	"github.com/pokt-foundation/utils-go/environment"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
 )
 
 const (
@@ -22,12 +25,18 @@ const (
 	maxBatchDuration = "MAX_BATCH_DURATION"
 	dbTimeout        = "DB_TIMEOUT"
 	debug            = "DEBUG"
+	useSSH           = "USE_SSH"
+	sshHost          = "SSH_HOST"
+	sshPort          = "SSH_PORT"
+	sshUser          = "SSH_USER"
+	sshKeyFilePath   = "SSH_KEY_FILE_PATH"
 
 	defaultPort          = "8080"
 	defaultBatchSize     = 1000
 	defaultBatchDuration = 60
 	defaultDBTimeout     = 60
 	defaultDebug         = false
+	defaultUseSSH        = false
 )
 
 type options struct {
@@ -38,6 +47,11 @@ type options struct {
 	maxBatchDuration time.Duration
 	dbTimeout        time.Duration
 	debug            bool
+	useSSH           bool
+	sshHost          string
+	sshPort          string
+	sshUser          string
+	sshKeyFilePath   string
 }
 
 func gatherOptions() options {
@@ -49,6 +63,11 @@ func gatherOptions() options {
 		maxBatchDuration: time.Duration(environment.GetInt64(maxBatchDuration, defaultBatchDuration)) * time.Second,
 		dbTimeout:        time.Duration(environment.GetInt64(dbTimeout, defaultDBTimeout)) * time.Second,
 		debug:            environment.GetBool(debug, defaultDebug),
+		useSSH:           environment.GetBool(useSSH, defaultUseSSH),
+		sshHost:          environment.GetString(sshHost, ""),
+		sshPort:          environment.GetString(sshPort, ""),
+		sshUser:          environment.GetString(sshUser, ""),
+		sshKeyFilePath:   environment.GetString(sshKeyFilePath, ""),
 	}
 }
 
@@ -66,9 +85,43 @@ func main() {
 		log.Level = logrus.DebugLevel
 	}
 
-	driver, err := postgresdriver.NewPostgresDriver(options.connectionString)
-	if err != nil {
-		panic(err)
+	var driver *postgresdriver.PostgresDriver
+
+	if options.useSSH {
+		sshKey, err := ioutil.ReadFile(options.sshKeyFilePath)
+		if err != nil {
+			panic(err)
+		}
+		sshKeySigner, err := ssh.ParsePrivateKey(sshKey)
+		if err != nil {
+			panic(err)
+		}
+
+		sshConfig := &ssh.ClientConfig{
+			User: options.sshUser,
+			Auth: []ssh.AuthMethod{
+				ssh.PublicKeys(sshKeySigner),
+			},
+			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		}
+
+		// Connect to the SSH Server
+		sshcon, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", options.sshHost, options.sshPort), sshConfig)
+		if err != nil {
+			panic(err)
+		}
+		defer sshcon.Close()
+
+		driver, err = postgresdriver.NewPostgresDriverWithSSH(options.connectionString, sshcon)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		var err error
+		driver, err = postgresdriver.NewPostgresDriver(options.connectionString)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	batch := batch.New(options.maxBatchSize, options.maxBatchDuration, options.dbTimeout, driver, log)
