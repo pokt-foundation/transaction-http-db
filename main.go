7fc865ff82fcf439a71155250a77baa1238ef782
commoddity <pascal@pokt.network>
2023-11-14 17:51:02 +0000

    feat: yse cloudsql config

--- main.go
+++ main.go
@@ -14,11 +14,22 @@ import (
 	"github.com/pokt-foundation/utils-go/environment"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"golang.org/x/crypto/ssh"
 )
 
 const (
-	connectionString              = "CONNECTION_STRING"
+	// App Env determines type of DB connection.
+	appEnv = "APP_ENV"
+	// Postgres DB vars - Required for all Envs.
+	pgUser     = "PG_USER"
+	pgPassword = "PG_PASSWORD"
+	pgDatabase = "PG_DATABASE"
+	// Local DB vars - Required for development/test Env.
+	pgHost = "PG_HOST"
+	pgPort = "PG_PORT"
+	// CloudSQL DB vars - Required for production Env.
+	dbInstanceConnectionName = "DB_INSTANCE_CONNECTION_NAME"
+
+	chanSize                      = "CHAN_SIZE"
 	apiKeys                       = "API_KEYS"
 	port                          = "PORT"
 	maxRelayBatchSize             = "MAX_RELAY_BATCH_SIZE"
@@ -27,12 +38,6 @@ const (
 	maxServiceRecordBatchDuration = "MAX_SERVICE_RECORD_BATCH_DURATION"
 	dbTimeout                     = "DB_TIMEOUT"
 	debug                         = "DEBUG"
-	useSSH                        = "USE_SSH"
-	sshHost                       = "SSH_HOST"
-	sshPort                       = "SSH_PORT"
-	sshUser                       = "SSH_USER"
-	sshKeyFilePath                = "SSH_KEY_FILE_PATH"
-	chanSize                      = "CHAN_SIZE"
 
 	defaultPort          = "8080"
 	defaultBatchSize     = 1000
@@ -43,28 +48,47 @@ const (
 	defaultChanSize      = 10000
 )
 
-type options struct {
-	connectionString              string
-	apiKeys                       map[string]bool
-	port                          string
-	maxRelayBatchSize             int
-	maxRelayBatchDuration         time.Duration
-	maxServiceRecordBatchSize     int
-	maxServiceRecordBatchDuration time.Duration
-	dbTimeout                     time.Duration
-	debug                         bool
-	useSSH                        bool
-	sshHost                       string
-	sshPort                       string
-	sshUser                       string
-	sshKeyFilePath                string
-	chanSize                      int
-}
+type (
+	options struct {
+		// Required vars
+		appEnv                                         string
+		pgHost, pgPort, pgUser, pgPassword, pgDatabase string
+		dbInstanceConnectionName                       string
+		apiKeys                                        map[string]bool
+		// Optional vars
+		port                          string
+		maxRelayBatchSize             int
+		maxRelayBatchDuration         time.Duration
+		maxServiceRecordBatchSize     int
+		maxServiceRecordBatchDuration time.Duration
+		dbTimeout                     time.Duration
+		debug                         bool
+		chanSize                      int
+	}
+
+	// DB config structs
+	DBConfig interface {
+		GetDriver(ctx context.Context) (driver *postgresdriver.PostgresDriver, cleanup func() error, err error)
+	}
+	CloudSQLConfig struct {
+		options
+	}
+	TestDBConfig struct {
+		options
+	}
+)
 
 func gatherOptions() options {
-	return options{
-		connectionString:              environment.MustGetString(connectionString),
-		apiKeys:                       environment.MustGetStringMap(apiKeys, ","),
+	appEnv := environment.GetString(appEnv, "production")
+
+	options := options{
+		appEnv: appEnv,
+		// Required vars
+		pgUser:     environment.MustGetString(pgUser),
+		pgPassword: environment.MustGetString(pgPassword),
+		pgDatabase: environment.MustGetString(pgDatabase),
+		apiKeys:    environment.MustGetStringMap(apiKeys, ","),
+		// Optional vars
 		port:                          environment.GetString(port, defaultPort),
 		maxRelayBatchSize:             int(environment.GetInt64(maxRelayBatchSize, defaultBatchSize)),
 		maxRelayBatchDuration:         time.Duration(environment.GetInt64(maxRelayBatchDuration, defaultBatchDuration)) * time.Second,
@@ -72,13 +96,58 @@ func gatherOptions() options {
 		maxServiceRecordBatchDuration: time.Duration(environment.GetInt64(maxServiceRecordBatchDuration, defaultBatchDuration)) * time.Second,
 		dbTimeout:                     time.Duration(environment.GetInt64(dbTimeout, defaultDBTimeout)) * time.Second,
 		debug:                         environment.GetBool(debug, defaultDebug),
-		useSSH:                        environment.GetBool(useSSH, defaultUseSSH),
-		sshHost:                       environment.GetString(sshHost, ""),
-		sshPort:                       environment.GetString(sshPort, ""),
-		sshUser:                       environment.GetString(sshUser, ""),
-		sshKeyFilePath:                environment.GetString(sshKeyFilePath, ""),
 		chanSize:                      int(environment.GetInt64(chanSize, defaultChanSize)),
 	}
+
+	switch appEnv {
+	case "production":
+		// For CloudSQL DB
+		options.dbInstanceConnectionName = environment.MustGetString(dbInstanceConnectionName)
+	default:
+		// For local DB
+		options.pgHost = environment.MustGetString(pgHost)
+		options.pgPort = environment.MustGetString(pgPort)
+	}
+
+	return options
+}
+
+// CloudSQLConfig.GetDriver connects to a GCP CloudSQL instance using the cloudsqlconn lib.
+// Intended for production use. Will be used if APP_ENV is 'production'.
+func (c *CloudSQLConfig) GetDriver(ctx context.Context) (driver *postgresdriver.PostgresDriver, cleanup func() error, err error) {
+	driverConfig := postgresdriver.CloudSQLConfig{
+		DBUser:                 c.options.pgUser,
+		DBPassword:             c.options.pgPassword,
+		DBName:                 c.options.pgDatabase,
+		InstanceConnectionName: c.options.dbInstanceConnectionName,
+		PrivateIP:              "true",
+	}
+
+	driver, cleanup, err = postgresdriver.NewCloudSQLPostgresDriver(driverConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return driver, cleanup, nil
+}
+
+// TestDBConfig.GetDriver connects to a Postgres database using standard connection string and user/PW.
+// Intended to be used for running tests on a local Docker container. Will be used if APP_ENV is 'test' or 'development'.
+func (c *TestDBConfig) GetDriver(ctx context.Context) (driver *postgresdriver.PostgresDriver, cleanup func() error, err error) {
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.options.pgHost,
+		c.options.pgPort,
+		c.options.pgUser,
+		c.options.pgPassword,
+		c.options.pgDatabase,
+	)
+
+	driver, cleanup, err = postgresdriver.NewPostgresDriver(connectionString)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return driver, cleanup, nil
 }
 
 func main() {
@@ -98,44 +167,27 @@ func main() {
 
 	log := zap.Must(logConfig.Build())
 
-	var driver *postgresdriver.PostgresDriver
-
-	if options.useSSH {
-		sshKey, err := os.ReadFile(options.sshKeyFilePath)
-		if err != nil {
-			panic(err)
-		}
-		sshKeySigner, err := ssh.ParsePrivateKey(sshKey)
-		if err != nil {
-			panic(err)
-		}
-
-		sshConfig := &ssh.ClientConfig{
-			User: options.sshUser,
-			Auth: []ssh.AuthMethod{
-				ssh.PublicKeys(sshKeySigner),
-			},
-			HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-		}
-
-		// Connect to the SSH Server
-		sshcon, err := ssh.Dial("tcp", fmt.Sprintf("%s:%s", options.sshHost, options.sshPort), sshConfig)
-		if err != nil {
-			panic(err)
-		}
-		defer sshcon.Close()
-
-		driver, err = postgresdriver.NewPostgresDriverWithSSH(options.connectionString, sshcon)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		var err error
-		driver, err = postgresdriver.NewPostgresDriver(options.connectionString)
-		if err != nil {
-			panic(err)
-		}
+	// Choose DB configuration based on APP_ENV
+	var dbConfig DBConfig
+	switch options.appEnv {
+	case "production":
+		// For CloudSQL DB
+		dbConfig = &CloudSQLConfig{options: options}
+	default:
+		// For local DB
+		dbConfig = &TestDBConfig{options: options}
 	}
+
+	driver, cleanup, err := dbConfig.GetDriver(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := cleanup(); err != nil {
+			log.Error(fmt.Sprintf("Failed to clean up: %v", err))
+		}
+	}()
 
 	relayBatch := batch.NewBatch(options.maxRelayBatchSize, options.chanSize, "relay", options.maxRelayBatchDuration, options.dbTimeout, driver.WriteRelays, log)
 	serviceRecordBatch := batch.NewBatch(options.maxServiceRecordBatchSize, options.chanSize, "service_record", options.maxServiceRecordBatchDuration, options.dbTimeout, driver.WriteServiceRecords, log)
